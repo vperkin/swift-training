9487f5201c54896e76387d2fbaf5100ac052baf2
Vyacheslav Perkin <103254910+vperkin@users.noreply.github.com>
2022-09-24 19:50:13 -0700

    Added 6.1 super function

---
+++ Homework.playground/Pages/6.1 summation.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+//: [Previous](@previous)
+
+import Foundation
+
+func summation(a: Int, b: Int) -> Int {
+    //super function, that takes two parameters as values for summation
+    //a and b are int values
+    a + b
+}
+
+print(summation(a: 2, b: 5))
+
+//: [Next](@next)
