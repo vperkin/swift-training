1c3feed79ef538d92a529bc6e673d28e7915242e
Vyacheslav Perkin <103254910+vperkin@users.noreply.github.com>
2022-10-03 02:03:54 -0700

    Minor update of Stack

---
+++ Homework.playground/Pages/13 Stack.xcplaygroundpage/Contents.swift
@@ -0,0 +1,29 @@
+//: [Previous](@previous)
+
+import Foundation
+
+struct Queue<Email> {
+    var emails: [Email] = []
+    
+    mutating func push(_ email:Email) {
+        emails.append(email)
+        print(emails)
+    }
+    
+    mutating func pop() -> Email? {
+        return emails.popLast()
+    }
+}
+
+var privateEmailId = Queue(emails: [11,12,13,14,15,16,17,18])
+
+privateEmailId.push(20)
+privateEmailId.pop()
+privateEmailId.push(301)
+
+
+//privateEmailId.output()
+
+
+
+//: [Next](@next)
