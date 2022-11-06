8c6704125171de96f03f6da187609ba3421a052b
Vyacheslav Perkin <103254910+vperkin@users.noreply.github.com>
2022-11-05 20:25:36 -0700

    ST minor update

---
+++ SImpleTransition/SImpleTransition/ColorTabbarVC.swift
@@ -0,0 +1,29 @@
+//
+//  ColorTabbarVC.swift
+//  SImpleTransition
+//
+//  Created by admin on 05.11.2022.
+//
+
+import UIKit
+
+class ColorTabbarVC: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SImpleTransition/SImpleTransition/OrangeVC.swift
@@ -0,0 +1,29 @@
+//
+//  OrangeVC.swift
+//  SImpleTransition
+//
+//  Created by admin on 05.11.2022.
+//
+
+import UIKit
+
+class OrangeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
